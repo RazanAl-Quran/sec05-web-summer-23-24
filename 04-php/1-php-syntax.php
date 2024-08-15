172fce1cd282a5ae7f35567372d664cf73244cdc
RazanAl-Quran <quraan_razan@hotmail.com>
2024-08-15 11:17:51 +0300

    feat: boostrap and php-01 classes have been added

---
+++ 04-php/1-php-syntax.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>  
+<?php
+    $username = 'sherry';
+    echo "<p>Hello from $username</p>";
+    ?>
+    <h1>Hello from my first PHP</h1>
+
+</body>
+</html>
